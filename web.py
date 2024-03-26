78d2d3c6a2c3996aa0fccbe03e52df62e6d4344f
Thomas <ttforsd@gmail.com>
2024-03-26 01:15:33 +0000

    udpated web texts

--- web.py
+++ web.py
@@ -8,10 +8,20 @@ llama = LlamaAPI(llama_key)
 # llm = ChatLlamaAPI(client=llama, temperature=0.1, model="llama-70b-chat")
 llm = GoogleGenerativeAI(model="gemini-1.0-pro", api_key=google_key)
 
-st.title('Nice CKS AI powered knowledge retrieval system')
+# set custom title
+st.set_page_config(page_title="Medibot", page_icon=":robot:", layout="wide")
 
-st.write("This is an AI powered knowledge retrieval system based on the National Institute for Health and Care Excellence (NICE) Clinical Knowledge Summaries (CKS).")
-st.write("Please do not use this system for medical advice. Always consult a healthcare professional for medical advice.")
+st.title('Nice CKS AI powered knowledge retrieval system')
+cks_url = "https://cks.nice.org.uk/"
+bnf_url = "https://bnf.nice.org.uk/treatment-summary/"
+
+
+# write a description of the system with embedded links for NICE CKS and NICE BNF
+
+st.markdown("This is an AI powered knowledge retrieval system based on the information from the [National Institute for Health and Care Excellence Clinical Knowledge Summaries (NICE CKS)](https://cks.nice.org.uk/) and [British National Formulary Treatment summaries (NICE BNF)](https://bnf.nice.org.uk/treatment-summary/)")
+
+
+st.write("Please do not use this system for medical advice. The answers generated may not be accurate. I am not responsible for any harm caused by the use of this system. Always consult a healthcare professional for medical advice.")
 
 with st.form(key='my_form'): 
     question = st.text_input(label='Question:')
